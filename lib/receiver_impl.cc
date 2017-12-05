4197f546fd19b664869707dc448f5ae0bb9bd6b6
Gilson Miranda Júnior <junior.kdm@gmail.com>
2017-12-05 03:34:21 -0200

    Added jitter calculation

--- lib/receiver_impl.cc
+++ lib/receiver_impl.cc
@@ -15,7 +15,22 @@ namespace gr {
 		}
 
 		receiver_impl::receiver_impl(const char *filename)
-			: gr::block("receiver", gr::io_signature::make(0, 0, 0), gr::io_signature::make(0, 0, 0)){
+			: gr::block("receiver", gr::io_signature::make(0, 0, 0), gr::io_signature::make(0, 0, 0)),
+			  stat_packets_total_received(0),
+			  stat_packets_valid_received(0),
+			  stat_packets_invalid_header(0),
+			  stat_packets_invalid_crc(0),
+			  stat_bytes_total_received(0),
+			  stat_bytes_valid_received(0),
+			  stat_throughput_average(0),
+			  stat_jitter_average(0),
+			  stat_latency_average(0),
+			  reception_started(boost::posix_time::not_a_date_time),
+			  reception_ended(boost::posix_time::not_a_date_time),
+			  last_packet_reception(boost::posix_time::not_a_date_time),
+			  last_packet_interval(boost::posix_time::not_a_date_time),
+			  aggregate_jitter(0),
+			  jitter_samples(0){
 
 			open_logfile(filename);
 			d_input_in_port = pmt::mp("input");
@@ -37,10 +52,26 @@ namespace gr {
 
 		bool receiver_impl::stop(){
 
-			// TODO store statistics
+			reception_ended = boost::posix_time::microsec_clock::local_time();
+			boost::posix_time::time_duration time_diff = reception_ended - reception_started;
+
+			stat_throughput_average = (stat_bytes_valid_received * 8.0) / ((double) time_diff.total_milliseconds()/1000.0);
+
+			stat_jitter_average = aggregate_jitter / jitter_samples;
+
+			d_logfile << "\n---- Finished ----\n" << std::flush;
+			d_logfile << "packets_total_received;packets_valid_received;packets_invalid_header;packets_invalid_crc;bytes_total_received;bytes_valid_received;average_throughput_bps;average_jitter_ms;duration_ms\n" << std::flush;
+			d_logfile << stat_packets_total_received << ";"
+					  << stat_packets_valid_received << ";"
+					  << stat_packets_invalid_header << ";"
+					  << stat_packets_invalid_crc << ";"
+					  << stat_bytes_total_received << ";"
+					  << stat_bytes_valid_received << ";"
+					  << stat_throughput_average << ";"
+					  << stat_jitter_average << ";"
+					  << time_diff.total_milliseconds() << "\n" << std::flush;
 
 			return block::stop();
-
 		}
 
 		void receiver_impl::open_logfile(const char *__filename){
@@ -55,7 +86,56 @@ namespace gr {
 
 		void receiver_impl::handle_incoming_packet(pmt::pmt_t __packet){
 
-			pmt::print(__packet);
+			packet *pk = new packet(__packet);
+
+			if (reception_started.is_not_a_date_time()){
+
+				reception_started = boost::posix_time::microsec_clock::local_time();
+			}
+
+			boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
+
+			stat_packets_total_received++;
+			stat_bytes_total_received += pk->get_message_length();
+
+			bool check_result = pk->check();
+
+			if (check_result == PK_VALID){
+
+				stat_packets_valid_received++;
+				stat_bytes_valid_received += pk->get_message_length();
+
+				if (!last_packet_reception.is_not_a_date_time()){
+
+					// Inteval between current and last packet
+					boost::posix_time::time_duration diff = now - last_packet_reception;
+					double current = (double)diff.total_milliseconds();
+					double last;
+
+					if (!last_packet_interval.is_not_a_date_time()){
+
+						last = (double)last_packet_interval.total_milliseconds();
+
+						// Difference of intervals
+						aggregate_jitter += fabs(current - last);
+						jitter_samples++;
+					}
+
+					last_packet_interval = diff;
+				} 
+
+				last_packet_reception = now;
+
+			} else if (check_result == PK_HEADER_ERR){
+
+				stat_packets_invalid_header++;
+
+			} else if (check_result == PK_CRC_ERR){
+
+				stat_packets_invalid_crc++;
+			}
+
+			delete pk;
 		}
 	} /* namespace trafficgen */
 } /* namespace gr */
--- lib/receiver_impl.h
+++ lib/receiver_impl.h
@@ -2,6 +2,7 @@
 #define INCLUDED_TRAFFICGEN_RECEIVER_IMPL_H
 
 #include <trafficgen/receiver.h>
+#include <trafficgen/packet.h>
 #include <iostream>
 #include <fstream>
 
@@ -12,6 +13,22 @@ namespace gr {
 
 		private:
 			std::ofstream d_logfile;
+			uint64_t stat_packets_total_received;
+			uint64_t stat_packets_valid_received;
+			uint64_t stat_packets_invalid_header;
+			uint64_t stat_packets_invalid_crc;
+			uint64_t stat_bytes_total_received;
+			uint64_t stat_bytes_valid_received;
+			double   stat_throughput_average;
+			double   stat_jitter_average;
+			double   stat_latency_average;
+
+			boost::posix_time::ptime reception_started;
+			boost::posix_time::ptime reception_ended;
+			boost::posix_time::ptime last_packet_reception;
+			boost::posix_time::time_duration last_packet_interval;
+			double   aggregate_jitter;
+			uint64_t jitter_samples;
 
 			pmt::pmt_t d_input_in_port;
 			pmt::pmt_t d_ack_out_port;
